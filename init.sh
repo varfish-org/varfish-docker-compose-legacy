b30c9effbe37dc56de02592ca778251138b6477a
root <root@varfish-build-release.cubi.bihealth.org>
2021-11-25 16:26:00 +0000

    docker-compose.yml and postgresql.conf updates

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-mkdir -p volumes/{exomiser,jannovar,minio,postgres/data,redis,traefik}
+mkdir -p volumes/{exomiser,jannovar,minio/varfish-server,postgres/data,redis,traefik}
+chmod -R ug=rwx,o=rx volumes/minio
+chown -R 995:994 volumes/minio
